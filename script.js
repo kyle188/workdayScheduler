eee51ad4a8c43d64b27c0e56391fbbda55b53e3b
omen <kybar089@gmail.com>
2022-10-02 10:13:52 -0500

    added renderColor function

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+var timeBlock = document.querySelector('.hour')
 var currentDate = moment().format('dddd, MMM Do')
-$("#currentDay").html(currentDate)
-
 var currentHour = moment().format('HH')
-console.log(currentHour)
+
+$("#currentDay").html(currentDate);
 
 
+
+function renderColors() {
+    $('.time-block').each(function() {
+    var blockHour = parseInt($(this).attr('id').split('hour')[0]);
+if (blockHour < currentHour) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+     $(this).addClass("past");
+} else if (blockHour === currentHour) {
+    $(this).removeClass("future");
+    $(this).removeClass("past");
+    $(this).addClass("present");
+} else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+}
+});
+}
+
+renderColors();
