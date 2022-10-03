035a02182a9ab6bae42aa5b0d2e6d41264670ad2
omen <kybar089@gmail.com>
2022-10-02 20:34:06 -0500

    created eventListener, localStorage now displayed on refresh

--- script.js
+++ script.js
@@ -4,24 +4,40 @@ var currentHour = moment().format('HH')
 
 $("#currentDay").html(currentDate);
 
+// Add Event Listener
+$(".saveBtn").on("click", function () {
+    var userText = $(this).siblings('.description').val();
+    var eventTime = $(this).parent().attr('id');
 
+    localStorage.setItem(eventTime, userText);
+})
+
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
 function renderColors() {
     $('.time-block').each(function() {
-    var blockHour = parseInt($(this).attr('id').split('hour')[0]);
-if (blockHour < currentHour) {
+    var blockHour = Number($(this).attr('id').split('hour')[0]);
+  if (blockHour < currentHour) {
     $(this).removeClass("future");
     $(this).removeClass("present");
-     $(this).addClass("past");
-} else if (blockHour === currentHour) {
+    $(this).addClass("past");
+  } else if (blockHour == currentHour) {
     $(this).removeClass("future");
     $(this).removeClass("past");
     $(this).addClass("present");
-} else {
+  } else {
     $(this).removeClass("present");
     $(this).removeClass("past");
     $(this).addClass("future");
-}
+  }
 });
 }
 
